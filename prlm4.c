9c480db15fc54bc5f9305e027d8d9afb7b0cabdf
Labagaran S <labagaran@gmail.com>
2026-01-27 09:40:24 +0530

    prlm5

---
+++ prlm4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() 
+{
+    int num;
+    int result;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    result = num / 8;
+    printf(" %d\n", result);
+    return 0;
+}
