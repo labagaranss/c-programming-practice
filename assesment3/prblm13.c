6939bdb0c593c7faa0d6efeef996b8b93b5a1fc3
Labagaran S <labagaran@gmail.com>
2026-01-27 12:25:11 +0530

    prblm1

---
+++ assesment3/prblm13.c
@@ -0,0 +1,23 @@
+
+
+#include <stdio.h>
+int main()
+{
+    int num,d1,d2;
+    scanf("%d",&num);
+    if(num<10||num>99)
+    {
+        return 0;
+    }
+    d1=num/10;
+    d2=num%10;
+    if(d1<=d2)
+    {
+        printf("success");
+    }
+    else
+    {
+         printf("failure");
+    }
+    return 0;
+}
---
+++ assesment3/prblm3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int num;
+    scanf("%d",&num);
+    if(num<=50)
+    {
+        printf("success");
+    }
+    else 
+    {
+        printf("failure");
+    }
+    return 0;
+}
