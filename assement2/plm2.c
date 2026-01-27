f5ce2d4a4bd40e71e78266695107e98ef09751bc
Labagaran S <labagaran@gmail.com>
2026-01-27 09:18:26 +0530

    pl1

---
+++ assement2/plm2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() 
+{
+    int num, result;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    result = num - 5;
+    printf(" %d\n", result);
+    return 0;
+}
